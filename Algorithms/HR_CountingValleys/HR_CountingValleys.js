4b8e64ad03dd276e83b48706b591df40600029a8
rob4abcba <rob_abcba@yahoo.com>
2020-02-10 20:28:29 -0800

    Solved HackerRank Counting Valleys

---
+++ Algorithms/HR_CountingValleys/HR_CountingValleys.js
@@ -0,0 +1,23 @@
+// Complete the countingValleys function below.
+function countingValleys(s) {
+    const characterArray = s.split("");
+    console.log("characterArray = ", characterArray);
+    let level = 0;
+    let valleys = 0;
+    let mountains = 0;
+    for (let character of characterArray) {
+        if (character == "D") {
+            if (level == 0) { valleys++; console.log("valleys = ", valleys); }
+            level--;
+
+        } else {
+            if (level == 0) { mountains++; console.log("mountains = ", mountains);  }
+            level++;
+        }
+        console.log("level = ", level);
+    } 
+    return valleys;
+}
+console.log("countingValleys('DUDUDU') = ", countingValleys('DUDUDU'));
+console.log("countingValleys('DDUDUUDU') = ", countingValleys('DDUDUUDU'));
+console.log("countingValleys('UDDDUDUUDU') = ", countingValleys('UDDDUDUUDU'));
