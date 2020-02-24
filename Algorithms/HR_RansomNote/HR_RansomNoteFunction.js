f57111f9d8f855ba1e96ca488c9ce02e32f23030
rob4abcba <rob_abcba@yahoo.com>
2020-02-24 12:19:10 -0800

    HashTable RansomNote Dict subset of oth dictionary

---
+++ Algorithms/HR_RansomNote/HR_RansomNoteFunction.js
@@ -0,0 +1,39 @@
+const ransomNote = (note, magazine) => {
+    console.log("note = ", note);
+    console.log("magazine = ", magazine);
+    const magazineWords = magazine.split(" ");
+    console.log("magazineWords = ", magazineWords);
+    const magazineHash = {};
+
+    magazineWords.forEach(word => {
+        console.log("word = ", word);
+        console.log("magazineHash[word] = ", magazineHash[word]);
+        if (!magazineHash[word]) magazineHash[word] = 0;
+        magazineHash[word]++;
+        console.log("magazineHash[word] = ", magazineHash[word]);
+    });
+
+    const noteWords = note.split(" ");
+    console.log("noteWords = ", noteWords);
+    let possible = true;
+
+    noteWords.forEach(word => {
+        console.log("word = ", word);
+        console.log("magazineHash[word] = ", magazineHash[word]);
+        if (magazineHash[word]) {
+            magazineHash[word]--;
+            console.log("magazineHash[word] = ", magazineHash[word]);
+            if (magazineHash[word] < 0) possible = false;
+        } else possible = false;
+    });
+
+    return possible;
+};
+
+const magazine =
+    "Here are some magazine words so you can cut out words for your ransom note dude";
+
+const note =
+    "Here is my ransom note";
+
+console.log("ransomNote(note, magazine) = ", ransomNote(note, magazine));
