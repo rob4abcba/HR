7d4110aefafef3d533c1f59f627ac93d993c439e
rob4abcba <rob_abcba@yahoo.com>
2021-02-04 00:07:16 -0800

    Add comments/explanation of finer details

--- Algorithms/Sort/BubbleSort.js
+++ Algorithms/Sort/BubbleSort.js
@@ -1,10 +1,14 @@
 function bubbleSort1(arr) {
     console.log("bubbleSort1: arr=" + arr);
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) { // MC: i increases from 0 up to (arr.length - 1)
         console.log("i="+i);
-        for (let j = 0; j < arr.length - i - 1; j++) {
+        for (let j = 0; j < arr.length - i - 1; j++) { // MC: As i increases, inner loop j decreases
+            // MC: Subtract 1 from end condition of loop because j = left number of pair
+            // and need to account for right number of pair being j+1 below.
+            // MC: 1st pass j incr fr 0 to (arr.length - 1)
             console.log("Compare pair at index j="+j, "arr[j]="+arr[j], "arr[j+1]="+arr[j+1]);
-            if (arr[j + 1] < arr[j]) {
+            // if (arr[j + 1] < arr[j]) {
+            if (arr[j] > arr[j + 1]) { // MC: King of Hill rises to the right (higher index) of array
                 console.log("Get ready to swap");
                 // ES6 way of swapping array elements
                 // [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
@@ -25,10 +29,10 @@ function bubbleSort1(arr) {
 function bubbleSort2(arr) {
     console.log("bubbleSort2: arr=" + arr);
     let noSwaps;
-    for (let i = arr.length; i > 0; i--) {
+    for (let i = arr.length; i > 0; i--) {  // MC: i decreases from arr.length down to 1.
         console.log("i="+i);
         noSwaps = true;
-        for (let j = 0; j < i - 1; j++) {
+        for (let j = 0; j < i - 1; j++) { // MC: 1st pass j incr fr 0 to (arr.length - 1)
             console.log("Compare pair at index j="+j, "arr[j]="+arr[j], "arr[j+1]="+arr[j+1]);
             if (arr[j + 1] < arr[j]) {
                 // Swap
@@ -42,6 +46,8 @@ function bubbleSort2(arr) {
             console.log("j loop: arr="+arr);
         }
         // End the iterations if there were no swaps made in one full pass
+        // MC: If no swaps for the bigger loop of j=0 to x
+        // then of course no swaps for the continually smaller loops j=0 to (x-1), then j=o to (x-2), etc.
         if (noSwaps) {
             console.log("No swaps made in one full pass. Time to break out of loop.")
             break;
